961c7676f391c2ad02bd713107572daaa15401ba
DarkAlekxx20 <niko.beelik@hotmail.com>
2023-02-22 19:20:08 -0600

    Mi primer proyecto

---
+++ src/israellópez_badacopu_a6p1/IsraelLópez_BADACOPU_A6P1.java
@@ -0,0 +1,9 @@
+package israellópez_badacopu_a6p1;
+/**
+ * @author nikob
+ */
+public class IsraelLópez_BADACOPU_A6P1{
+    public static void main(String[] args){
+        System.out.println("Hola mundo desde GitHub :)");
+    }   
+}
